ad5b625689bd918ff1f7fa7933686adf12f56a29
Norbert Luszkiewicz <norbert.luszkiewicz@stxnext.pl>
2023-03-12 09:21:01 +0100

    add 2 new scopes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   url: 'https://dynamix-bot.glitch.me/',
   loginRedirect:
-    'https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=bhwlcwuvtg51226poslegrqdcm8naz&redirect_uri=https://dynamix-bot.glitch.me/register&scope=viewing_activity_read channel:moderate channel:manage:redemptions channel:read:redemptions moderation:read user:read:email chat:edit chat:read&state=localhost',
+    'https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=bhwlcwuvtg51226poslegrqdcm8naz&redirect_uri=https://dynamix-bot.glitch.me/register&scope=viewing_activity_read channel:moderate channel:manage:redemptions channel:read:redemptions moderation:read channel:manage:predictions moderator:manage:banned_users user:read:email chat:edit chat:read&state=localhost',
 };
 
 /*
