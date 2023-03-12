6db8c460ad029b4a4c3db4ca24ee5ce015c4b6f3
Norbert Luszkiewicz <norbert.luszkiewicz@stxnext.pl>
2023-03-12 09:09:56 +0100

    add moderation:read scope

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   url: 'https://dynamix-bot.glitch.me/',
   loginRedirect:
-    'https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=bhwlcwuvtg51226poslegrqdcm8naz&redirect_uri=https://dynamix-bot.glitch.me/register&scope=viewing_activity_read channel:moderate channel:manage:redemptions channel:read:redemptions user:read:email chat:edit chat:read&state=c3ab8aa609ea11e793ae92361f002671',
+    'https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=bhwlcwuvtg51226poslegrqdcm8naz&redirect_uri=https://dynamix-bot.glitch.me/register&scope=viewing_activity_read channel:moderate channel:manage:redemptions channel:read:redemptions moderation:read user:read:email chat:edit chat:read&state=c3ab8aa609ea11e793ae92361f002671',
 };
